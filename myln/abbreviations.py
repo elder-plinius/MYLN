22ea3dda1237275ee83c68bd0bc785922d5aff78
pliny <133052465+elder-plinius@users.noreply.github.com>
2024-02-10 12:56:56 -0800

    Create abbreviations.py

---
+++ myln/abbreviations.py
@@ -0,0 +1,36 @@
+class Abbreviations:
+    default_abbreviations = {
+        "as soon as possible": "ASAP",
+        "for your information": "FYI",
+        "and": "&",
+        "appointment": "appt",
+        "approximately": "approx",
+        "department": "dept",
+        "information": "info",
+        "introduction": "intro",
+        "number": "#",
+        "percent": "%",
+        "question": "Q",
+        "answer": "A",
+        "reference": "ref",
+        "without": "w/o",
+        "with": "w/",
+        "example": "e.g.",
+        "that is": "i.e.",
+        "please": "pls",
+        "thanks": "thx",
+        "thank you": "ty",
+        "application": "app",
+        "technology": "tech",
+        "university": "univ",
+        "versus": "vs",
+        "estimate": "est",
+        "demonstration": "demo",
+        "government": "govt"
+    }
+
+    @classmethod
+    def apply_abbreviations(cls, text):
+        for phrase, abbreviation in cls.default_abbreviations.items():
+            text = text.replace(phrase, abbreviation)
+        return text
